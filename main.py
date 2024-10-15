8173b6c06da26d177f1241d036449055e0c89392
Ariba <102295115+ariba-work@users.noreply.github.com>
2024-10-15 23:48:37 +0200

    unfolding step in classic

--- main.py
+++ main.py
@@ -1,63 +1,119 @@
+import csv
+import signal
+import time
 from os.path import join
+
 
 import click as click
 from pm4py.objects.log.importer.xes.importer import apply as xes_import
+from pm4py.objects.log.util.interval_lifecycle import to_interval
 from pm4py.objects.petri_net.importer import importer as petri_importer
 from pm4py.objects.petri_net.utils.synchronous_product import construct as construct_synchronous_product
+from pm4py.util.xes_constants import DEFAULT_START_TIMESTAMP_KEY
 
-from util.constants import SearchAlgorithms, AlternatingMethod, TerminationCriterion, SplitPointSolvers, \
-    PartialOrderMode, SKIP
-from util.petri_net_utils import get_partial_trace_net_from_trace, get_partial_order_relations_from_trace
-from util.tools import compute_order_aware_alignments_for_sync_product_net, ExecutionVariant
-from util.visualization_tools import draw_order_aware_alignment
+from util.alignment_utils import construct_unfolded_net_from_alignment
+from util.constants import SearchAlgorithms, PartialOrderMode, SKIP
+from util.petri_net_utils import get_partial_trace_net_from_trace
+from util.tools import ExecutionVariant, \
+    compute_alignments_for_sync_product_net
+
+
+header = ['variant', 'trace_idx', 'trace_length', 'time_taken', 'time_taken_potext', 'queued_events', 'visited_events', 'alignment_costs']
+
+
+# Define a timeout handler
+def timeout_handler(signum, frame):
+    raise TimeoutError("Trace processing timed out")
+
+# Set the timeout duration (in seconds)
+timeout_duration = 3  # Adjust as needed
+
+# Register the timeout handler
+signal.signal(signal.SIGALRM, timeout_handler)
+
+
+def process_trace(trace_idx, trace, model_net, model_im, model_fm):
+    try:
+        # Set the alarm
+        signal.alarm(timeout_duration)
+
+        # build trace net
+        net, im, fm = get_partial_trace_net_from_trace(trace, PartialOrderMode.REDUCTION, False)
+
+        print(f'{trace_idx}::')
+
+        # build SPN
+        sync_prod, sync_im, sync_fm = construct_synchronous_product(net, im, fm, model_net, model_im, model_fm, SKIP)
+
+        a = compute_alignments_for_sync_product_net(sync_prod, sync_im, sync_fm, ExecutionVariant(search_algorithm=SearchAlgorithms.A_STAR))
+
+        unfolding_start_time = time.time()
+        _, _, _ = construct_unfolded_net_from_alignment(a, sync_prod, sync_im)
+
+        output = [
+            0,
+            trace_idx,
+            len(trace),
+            time.time() - unfolding_start_time + a.total_duration,
+            'N/A',
+            a.queued_states,
+            a.visited_states,
+            a.alignment_costs,
+        ]
+
+        return output
+
+    except TimeoutError:
+        print(f"Trace {trace_idx} processing timed out")
+        return [
+            0,
+            trace_idx,
+            len(trace),
+            "timeout",
+            "timeout",
+            "timeout",
+            "timeout",
+            "timeout",
+        ]
+
+    finally:
+        # Disable the alarm
+        signal.alarm(0)
 
 
 @click.command()
 @click.option('--path', '-p', help='Path to the data directory.')
 @click.option('--log', '-l', help='Name of the event log.')
 @click.option('--model', '-m', help='Name of the process model.')
-def computeorderawarealignments(path: str, log: str, model: str):
-    model_net, model_im, model_fm = petri_importer.apply(join(path, model))
-    event_log = xes_import(join(path, log))
+def compute_classic_alignments(path: str, log: str, model: str):
 
-    variants_to_execute = [
-        ExecutionVariant(search_algorithm=SearchAlgorithms.DIJKSTRA),
-        ExecutionVariant(search_algorithm=SearchAlgorithms.DIJKSTRA_BIDIRECTIONAL,
-                         bidirectional_alternating=AlternatingMethod.STRICTLY_ALTERNATE),
-        ExecutionVariant(search_algorithm=SearchAlgorithms.A_STAR),
-        ExecutionVariant(search_algorithm=SearchAlgorithms.A_STAR_BIDIRECTIONAL,
-                         bidirectional_alternating=AlternatingMethod.STRICTLY_ALTERNATE,
-                         bidirectional_termination=TerminationCriterion.SYMMETRIC_LOWER_BOUNDING),
-        ExecutionVariant(search_algorithm=SearchAlgorithms.SPLIT_POINT,
-                         split_point_solver=SplitPointSolvers.GUROBI),
-        ExecutionVariant(search_algorithm=SearchAlgorithms.SPLIT_POINT_BIDIRECTIONAL,
-                         bidirectional_alternating=AlternatingMethod.STRICTLY_ALTERNATE,
-                         bidirectional_termination=TerminationCriterion.SYMMETRIC_LOWER_BOUNDING,
-                         split_point_solver=SplitPointSolvers.GUROBI),
-    ]
+    model_net, model_im, model_fm = petri_importer.apply(join(f'.', path, model))
+    event_log = xes_import(join(f'.', path, log))
 
-    for trace_idx, trace in enumerate(event_log, 1):
-        # build trace net
-        net, im, fm = get_partial_trace_net_from_trace(trace, PartialOrderMode.REDUCTION, False)
-        trace_order_relations = get_partial_order_relations_from_trace(trace)
+    if (
+            DEFAULT_START_TIMESTAMP_KEY not in event_log[0][0]
+    ):
+        event_log = to_interval(event_log)[:1]
 
-        # build SPN
-        sync_prod, sync_im, sync_fm = construct_synchronous_product(net, im, fm, model_net, model_im, model_fm, SKIP)
+    print(f'total number of traces: {len(event_log)}')
 
-        # compute alignments
-        for var in variants_to_execute:
-            a = compute_order_aware_alignments_for_sync_product_net(sync_prod, sync_im, sync_fm, var,
-                                                                    trace_dependencies=trace_order_relations)
-            print(f"{var}: {a}")
-            draw_order_aware_alignment(a, f"alignment-{trace_idx}-{var}", path)
+    with open(f'experiments/results/{model}.csv', mode='a', newline='') as output:
+        writer = csv.writer(output)
+
+        for trace_idx, trace in enumerate(event_log, 1):
+            result = process_trace(trace_idx, trace, model_net, model_im, model_fm)
+            writer.writerow(result)
+
+        print(f'completed for model={model}, closing file')
+
+    output.close()
 
 
 @click.group()
 def cli():
     pass
 
-
-cli.add_command(computeorderawarealignments, "compute-order-aware-alignments")
+cli.add_command(compute_classic_alignments, "classic-alignments")
 
 if __name__ == '__main__':
     cli()
--- util/alignment_utils.py
+++ util/alignment_utils.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from pm4py.objects.petri_net.obj import Marking, PetriNet
+from pm4py.objects.petri_net.utils.petri_utils import add_arc_from_to
 
 from util import constants
 from util.constants import DEFAULT_DEPENDENCY_COSTS
@@ -34,7 +35,7 @@ class AlignmentResult:
         # reconstruct alignment
         s = state
         while s.p is not None:
-            alignment = [(s.unique_transition_id, s.t.label)] + alignment
+            alignment = [s.t] + alignment
             markings = [s.m] + markings
             s = s.p
 
@@ -196,3 +197,93 @@ def __get_transitions_to_visit_str_cost(current_marking: Marking, trans_empty_pr
 
     return [(t, cost_function[t.name]) for t in enabled_trans
             if not (t is not None and is_log_move(t, constants.SKIP) and is_model_move(t, constants.SKIP))]
+
+
+def unfolding_step(idx: int, alignment_result: AlignmentResult, net: PetriNet, sync_prod: PetriNet, curr_marking: Marking,
+                   curr_marking_unf: Marking, map_clone_2_orig, map_clone_place_2_orig, map_orig_place_2_clone,
+                   num_conditions: int):
+
+    # find transition in spn
+    t_alignment = alignment_result.alignment[idx]
+    t_spn = None
+
+    for t in sync_prod.transitions:
+        if t.name == t_alignment.name:
+            t_spn = t
+
+    if t_spn:
+        tu = PetriNet.Transition(t_spn.name, t_spn.label)
+        net.transitions.add(tu)
+
+        # append t and t* to unfolded net
+        for in_arc in t_spn.in_arcs:
+            orig_in_place = in_arc.source
+            clone_in_place = map_orig_place_2_clone.get(orig_in_place)
+
+            if clone_in_place in curr_marking_unf and curr_marking_unf.get(clone_in_place) > 0:
+                add_arc_from_to(clone_in_place, tu, net)
+                curr_marking_unf.pop(clone_in_place)
+                curr_marking.pop(orig_in_place)
+                # curr_marking_unf[clone_in_place] -= 1
+                # curr_marking[orig_in_place] -= 1
+
+            else:
+                print(f'Error: Found a transition {t}, *p={clone_in_place}  enabled in synchronized model but not enabled in unfold net.')
+
+
+        # Put new conditions of out places in unfold net.
+
+        for out_arc in t_spn.out_arcs:
+            orig_out_place = out_arc.target
+            clone_out_place = PetriNet.Place(orig_out_place.name)
+            num_conditions += 1
+
+            net.places.add(clone_out_place)
+
+            add_arc_from_to(tu, clone_out_place, net)
+            map_orig_place_2_clone[orig_out_place] = clone_out_place
+            map_clone_place_2_orig[clone_out_place] = orig_out_place
+            curr_marking_unf[clone_out_place] = 1
+            curr_marking[orig_out_place] = 1
+
+    else:
+        raise Exception('No enabled transition')
+
+    # Added arcs (p, t) to unfoldnet
+
+    if idx < len(alignment_result.alignment) - 1:
+        unfolding_step(idx + 1, alignment_result, net, sync_prod, curr_marking, curr_marking_unf, map_clone_2_orig,
+                       map_clone_place_2_orig, map_orig_place_2_clone, num_conditions)
+
+
+def construct_unfolded_net_from_alignment(alignment_result: AlignmentResult, sync_prod: PetriNet, im: Marking):
+    net = PetriNet()
+    net_im = Marking()
+
+    map_clone_2_orig = {}  # Transition to Transition
+    map_clone_place_2_orig = {}  # Place to Place
+    map_orig_place_2_clone = {}  # Place to Place
+
+    # create B_0
+
+    i = 0
+    curr_marking_unf = Marking()  # the current marking of unfoldnet
+    curr_marking = Marking()  # the current marking of synchron model
+    num_conditions = 0
+
+    for p in im:
+        newp = PetriNet.Place(p.name)
+        num_conditions += 1
+        net.places.add(newp)
+        i += 1
+        net_im[newp] = 1
+        map_clone_place_2_orig[newp] = p
+        map_orig_place_2_clone[p] = newp
+        curr_marking_unf[newp] = 1
+        curr_marking[p] = 1
+
+    # unfolding step
+    unfolding_step(0, alignment_result, net, sync_prod, curr_marking, curr_marking_unf, map_clone_2_orig, map_clone_place_2_orig,
+                   map_orig_place_2_clone, num_conditions)
+
+    return net, net_im, curr_marking_unf
